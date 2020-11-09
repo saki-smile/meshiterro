119fd50b629918b286580a012eb047fd34f2f09e
saki-smile <sa.su.ko0419@gmail.com>
2020-11-09 02:01:48 +0000

    [Add] indexページ

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'cd6b370c5aecc74b33623f2905314e70cf0c1b8e6377dfd2f4cbfd0d421c4b5a675945a7b0beebb75547d895d1a3134631ac39735530aad760ab41db0a6a8425'
